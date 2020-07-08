d3f7e2170f16bd851c5b65f414d5529987915c51
niyaz <niyazmannapov@gmail.com>
2020-07-08 16:02:39 +0300

    initial

---
+++ src/main/java/ru/itis/ivavprp/IvavprpApplication.java
@@ -0,0 +1,13 @@
+package ru.itis.ivavprp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IvavprpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IvavprpApplication.class, args);
+    }
+
+}
